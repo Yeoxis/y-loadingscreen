4d25e639e9f08a2ef22067e7d26a4764b3df31ef
Yeox <115135755+Yeoxis@users.noreply.github.com>
2026-03-28 03:25:47 -0400

    Update fxmanifest.lua
    change pattern to call all files in assets folder opposed to adding them all individually.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,12 +12,5 @@ disable_loading_screens 'yes'
 
 files {
   'index.html',
-  'assets/logo.png',
-  'assets/song1.mp3',
-  'assets/song2.mp3',
-  'assets/song3.mp3',
-  'assets/bg.mp4',
-  'assets/slide1.jpg',
-  'assets/slide2.jpg',
-  'assets/slide3.jpg',
+  'assets/*',
 }
